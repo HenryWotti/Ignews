b705141701f92708e581b76a33f76702ea3e54f4
henrycky <henrycky_wottikosky@hotmail.com>
2022-05-04 09:15:35 -0300

    colocando a home em SSG-Static

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import { SubscribeButton } from '../components/SubscribeButton'
 import { stripe } from '../services/stripe';
-import { GetServerSideProps } from 'next';
+import { GetStaticProps } from 'next';
 import styles from './home.module.scss'
 import Head from 'next/head'
 
@@ -37,7 +37,7 @@ export default function Home({ product }: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async() => {
+export const getStaticProps: GetStaticProps = async() => {
  
   const price = await stripe.prices.retrieve('price_1KvAjgDDv2Syf6KENKSF6DXJ')
 
@@ -52,7 +52,7 @@ export const getServerSideProps: GetServerSideProps = async() => {
   return {
     props: {
       product,
-
-    }
+    },
+    revalidate: 60*60*24, // 24 hours
   }
 }
